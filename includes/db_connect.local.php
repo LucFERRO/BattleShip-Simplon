08d33a7a8c4dc363d8f1b1cf0847fbc97a89387d
Luc FERRO <86306667+LucFERRO@users.noreply.github.com>
2022-05-12 17:11:17 +0200

    Update db_connect.local.php

--- includes/db_connect.local.php
+++ includes/db_connect.local.php
@@ -2,9 +2,9 @@
 
 function connexion(){
     $servername = "localhost";
-    $username = "u983603541_Sakou";
-    $password = "B87ce6a6a1e!";
-    $dbname = "u983603541_BattleShip";
+    $username = "LOLOL";
+    $password = "LOLOLO!";
+    $dbname = "LOLOLOL";
     try{
         $db = new PDO(
             'mysql:host='.$servername.';dbname='.$dbname.';charset=UTF8',
